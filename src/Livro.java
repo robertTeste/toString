c8bc428025b152b5710c40dfacacdb90e4a91323
robertTeste <robert.geison100@gmail.com>
2026-05-18 19:36:36 -0400

    exercicio 5

---
+++ src/Livro.java
@@ -0,0 +1,9 @@
+class Livro {
+    private String titulo;
+    private String isbn;
+
+    public Livro(String titulo, String isbn) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Produto produto1 = new Produto("Caneta", 2.50);
-        Produto produto2 = new Produto("Caderno", 15.90);
+        // Instanciando dois objetos com os mesmos dados
+        Livro l1 = new Livro("Clean Code", "978-0132350884");
+        Livro l2 = new Livro("Clean Code", "978-0132350884");
 
-        System.out.println(produto1);
-        System.out.println(produto2);
+        System.out.println(l1 == l2);       // false
+        System.out.println(l1.equals(l2));  // false
 
-        System.out.println("Item: " + produto1);
-
-        /*
-         O que mudou na saída?
-         Agora, em vez de exibir aquele código confuso com o endereço de memória
-         (Ex: Produto@1540e19d), o console exibe o texto formatado e legível que
-         definimos dentro do método toString():
-         Produto{nome='Caneta', preco=2.5}
-         Produto{nome='Caderno', preco=15.9}
-         O toString() é chamado automaticamente?
-         Sim! O método toString() é invocado de forma automática pelo Java em três
-         situações principais demonstradas aqui:
-         1. Dentro do método System.out.println(objeto), pois ele precisa converter
-         o objeto em texto antes de exibir.
-         2. Na concatenação usando o operador '+' ("Item: " + produto1). O Java
-         chama o toString() por debaixo dos panos para unir o texto com o objeto.
-         3. No painel de variáveis da IDE/Debugger durante a inspeção de código.
-         */
+        // Resposta 1: Ambas as saídas são 'false' porque 'l1' e 'l2' são dois objetos diferentes criados em posições distintas da memória Heap.
+        // Resposta 2: O método '.equals()' sem override (padrão herdado da classe Object) se comporta exatamente como o operador '==', comparando apenas os endereços de memória dos objetos, ignorando o conteúdo dos atributos.
     }
 }
